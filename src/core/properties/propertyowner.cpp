5ad43a95154d4611f4919034a5a28396b9459cf6
Inviwo-Team <team@inviwo.org>
2015-03-11 10:12:53 +0100

    Core (Various bugfixes and C++11 related updates), QtWidgets sizing/formatting updates

--- src/core/properties/propertyowner.cpp
+++ src/core/properties/propertyowner.cpp
@@ -109,6 +109,7 @@ Property* PropertyOwner::removeProperty(std::vector<Property*>::iterator it) {
             cit = std::find(compositeProperties_.begin(),compositeProperties_.end(), *it);
         if (cit != compositeProperties_.end()) compositeProperties_.erase(cit);
 
+        prop->setOwner(nullptr);
         properties_.erase(it);
         notifyObserversDidRemoveProperty(prop, index);
     }
@@ -117,11 +118,11 @@ Property* PropertyOwner::removeProperty(std::vector<Property*>::iterator it) {
 
 Property* PropertyOwner::getPropertyByIdentifier(const std::string& identifier,
                                                  bool recursiveSearch) const {
-    for (auto& property : properties_) {
+    for (Property* property : properties_) {
         if (property->getIdentifier() == identifier) return property;
     }
     if (recursiveSearch) {
-        for (auto compositeProperty : compositeProperties_) {
+        for (CompositeProperty* compositeProperty : compositeProperties_) {
             Property* p = compositeProperty->getPropertyByIdentifier(identifier, true);
             if (p) return p;
         }
